d736626ded1bceddf48ba43dc02d968c61049d4d
tomymusic <tomasnumber1@gmail.com>
2025-02-25 23:55:27 -0300

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,4 +1,7 @@
 "use client";
-export default function Input(props) {
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export default function Input(props: InputProps) {
   return <input {...props} className="border p-2 rounded" />;
 }
