a352042af83b7d40c43dfdaf1ed54c50bfa6c125
tomymusic <tomasnumber1@gmail.com>
2025-02-25 23:48:15 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import { ImagePlayground } from "@/components/ImagePlayground";
 import { getRandomSuggestions } from "@/lib/suggestions";
+import ImageUploader from "@/components/ImageUploader";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <ImagePlayground suggestions={getRandomSuggestions()} />;
+  return (
+    <div>
+      <ImageUploader />  {/* Agregamos el componente de subida de imágenes */}
+      <ImagePlayground suggestions={getRandomSuggestions()} />
+    </div>
+  );
 }
