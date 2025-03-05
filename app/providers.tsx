852a0eeca168f880a82a30f9837db07332100991
tomymusic <tomasnumber1@gmail.com>
2025-03-04 22:02:34 -0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,10 +7,11 @@ import "@shopify/polaris/build/esm/styles.css";
 
 export default function Providers({ children }: { children: ReactNode }) {
     const searchParams = typeof window !== "undefined" ? new URLSearchParams(window.location.search) : null;
-    const host = searchParams?.get("host") ?? "";
+    const host: string = searchParams?.get("host") ?? "";
+    const apiKey: string = process.env.NEXT_PUBLIC_SHOPIFY_API_KEY ?? "";
 
     return (
-        <AppBridgeProvider config={{ apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY, host }}>
+        <AppBridgeProvider config={{ apiKey, host }}>
             <PolarisProvider>
                 {children}
             </PolarisProvider>
