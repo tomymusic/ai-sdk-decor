d7cab90fab4daeb721a4b5eec15619d626626099
tomymusic <tomasnumber1@gmail.com>
2025-03-04 21:41:28 -0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { ReactNode } from "react";
 import { Provider as AppBridgeProvider } from "@shopify/app-bridge-react";
 import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 
-export default function Providers({ children }) {
+export default function Providers({ children }: { children: ReactNode }) {
     const host = typeof window !== "undefined" ? new URLSearchParams(window.location.search).get("host") : "";
 
     return (
