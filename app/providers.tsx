ad5a86b83ee28569ccb5f4627b7580b2e196ed6d
tomymusic <tomasnumber1@gmail.com>
2025-03-04 22:05:15 -0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -12,7 +12,7 @@ export default function Providers({ children }: { children: ReactNode }) {
 
     return (
         <AppBridgeProvider config={{ apiKey, host }}>
-            <PolarisProvider>
+            <PolarisProvider i18n={{ locale: "en" }}>
                 {children}
             </PolarisProvider>
         </AppBridgeProvider>
