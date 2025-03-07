2366072964335f1fbd020e2656ce9382c6b7060e
tomymusic <tomasnumber1@gmail.com>
2025-03-07 17:28:52 -0300

    Update ImageUploader.tsx

--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 
 interface ImageUploaderProps {
   onImageUpload: (imageUrl: string | null) => void;
 }
 
 export function ImageUploader({ onImageUpload }: ImageUploaderProps) {
-  const [preview, setPreview] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,7 +29,6 @@ export function ImageUploader({ onImageUpload }: ImageUploaderProps) {
 
       if (response.ok) {
         onImageUpload(data.imageUrl);
-        setPreview(data.imageUrl);
       } else {
         console.error("❌ Error uploading image:", data.error);
       }
@@ -54,16 +51,11 @@ export function ImageUploader({ onImageUpload }: ImageUploaderProps) {
               type="file"
               accept="image/*"
               onChange={handleImageChange}
-              className="block w-full cursor-pointer text-sm bg-transparent border-none p-2 rounded-md text-[#111111] focus-visible:ring-0 focus-visible:ring-offset-0 file:bg-black file:text-white file:border-none file:rounded-lg file:px-4 file:py-1 file:text-sm"
+              className="block w-full cursor-pointer text-sm bg-transparent border-none p-2 rounded-md text-[#111111] focus-visible:ring-0 focus-visible:ring-offset-0 file:bg-black file:text-white file:border-none file:rounded-lg file:px-3 file:py-1.5 file:text-sm"
             />
           </div>
         </div>
         {loading && <p className="text-sm text-zinc-500 mt-2">Uploading...</p>}
-        {preview && (
-          <div className="mt-4 w-40 rounded-lg overflow-hidden">
-            <Image src={preview} alt="Preview" width={160} height={160} className="rounded-lg" />
-          </div>
-        )}
       </div>
     </div>
   );
