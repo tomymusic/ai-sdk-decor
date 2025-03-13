873ffc52660f8e45b72c608c67fe48036b965363
tomymusic <tomasnumber1@gmail.com>
2025-03-13 12:42:55 -0300

    Update ImagePlayground.tsx

--- components/ImagePlayground.tsx
+++ components/ImagePlayground.tsx
@@ -78,7 +78,7 @@ export function ImagePlayground({ suggestions = [] }: ImagePlaygroundProps) {
   };
 
   return (
-    <div className="min-h-screen bg-background py-8 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-background">
       <div className="max-w-7xl mx-auto">
         <Header />
         <ImageUploader onImageUpload={handleImageUpload} />
